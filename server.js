b006a13a992a0ddaf41d21045d7756464c86b766
saurabhrailinfo-svg <saurabh.rail.info@gmail.com>
2026-04-14 00:39:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.get("/", (req, res) => {
   res.send("Server Running ✅");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log("Server running on port " + (process.env.PORT || 3000));
 });
