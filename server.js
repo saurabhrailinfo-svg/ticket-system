5ce714a9a1cab39fbac5035939d05fa40c6f0476
saurabhrailinfo-svg <saurabh.rail.info@gmail.com>
2026-04-14 01:18:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get("/ping", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-// CRITICAL FIX
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
