8546d373e308877f2769eaf0d227a572f29b992b
saurabhrailinfo-svg <saurabh.rail.info@gmail.com>
2026-04-14 01:22:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const app = express();
 
 app.use(express.json());
 
-// MUST: fast response for Railway
+let orders = [];
+
+// Home
 app.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.send("OK");
 });
 
-// test route
+// Test
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
+});
+
+// Save ticket
+app.post("/save", (req, res) => {
+  orders.push(req.body);
+  res.json({ success: true, data: req.body });
+});
+
+// Get all orders
+app.get("/orders", (req, res) => {
+  res.json(orders);
 });
 
 const PORT = process.env.PORT || 3000;
