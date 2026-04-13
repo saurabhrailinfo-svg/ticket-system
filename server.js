f4bdcb012ddb35f361d37689e36b9228bcdc99f4
saurabhrailinfo-svg <saurabh.rail.info@gmail.com>
2026-04-13 23:55:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let orders = [];
+
+app.post("/save", (req, res) => {
+  orders.push(req.body);
+  res.send("Saved ✅");
+});
+
+app.get("/orders", (req, res) => {
+  res.json(orders);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server Running ✅");
+});
+
+app.listen(3000, () => console.log("Server running"));
